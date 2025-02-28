b063879e8e7f710c6242e08af2a5adc91fcea93b
50Mph <somphongchanthamixay01@gmail.com>
2025-02-28 22:56:05 +0700

    a

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/50Mph/go-api/config"
+)
+
+func main() {
+
+	ctx := context.Background()
+	_ = ctx
+
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+	log.Println(cfg)
+}
