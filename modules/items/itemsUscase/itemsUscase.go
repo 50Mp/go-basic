beab381cb28439d293fd8fa39f9c98a214eb7306
50Mp <somphongchanthamixay01@gmail.com>
2025-04-05 21:43:13 +0700

    add funtion modules items

--- modules/items/itemsUscase/itemsUscase.go
+++ modules/items/itemsUscase/itemsUscase.go
@@ -1,1 +1,16 @@
 package itemsuscase
+
+import (
+	itemsrepository "github.com/50Mph/go-api/modules/items/itemsRepository"
+)
+
+type (
+	ItemsUcase interface{}
+	itemsUcase struct {
+		itemsRepository itemsrepository.ItemsRepository
+	}
+)
+
+func NewItemsUscae(itemsRepository itemsrepository.ItemsRepository) ItemsUcase {
+	return itemsUcase{itemsRepository: itemsRepository}
+}
